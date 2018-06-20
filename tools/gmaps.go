fb161a20a81bd7869c3f9712b14b41298cb3f5de
ericdfournier <me@ericdfournier.com>
2018-06-20 16:45:58 -0700

    More minor fixes.

--- tools/gmaps.go
+++ tools/gmaps.go
@@ -46,33 +46,33 @@ func main() {
 	}
 	gmaps.HelpName = "gmaps"
 	gmaps.Commands = []cli.Command{
-        // Geocoder API Sub-Command
-        {
-            Name: "geocode",
+		// Geocoder API Sub-Command
+		{
+			Name:    "geocode",
 			Aliases: []string{"gc"},
-			Usage: "Google Maps Geocoder API Tool",
+			Usage:   "Google Maps Geocoder API Tool",
 			Description: `Accepts an Input CSV File With Address Strings and 
 			Outputs Formatted CSV File With Geocoder Response 
 			Latitude, Longitude Results.`,
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name: "key, k",
-					Usage: "Google Maps Geocoder API 'Key'",
-					Value: apiKey,
-                    EnvVar: "GMAPS_API_KEY",
+					Name:   "key, k",
+					Usage:  "Google Maps Geocoder API 'Key'",
+					Value:  apiKey,
+					EnvVar: "GMAPS_API_KEY",
 				},
 				cli.StringFlag{
-					Name: "input, i",
+					Name:  "input, i",
 					Usage: "Input 'Filepath'",
 					Value: input,
 				},
 				cli.StringFlag{
-					Name: "output, o",
+					Name:  "output, o",
 					Usage: "Output 'Directory Path'",
 					Value: output,
 				},
 				cli.StringFlag{
-					Name: "region, r",
+					Name:  "region, r",
 					Usage: "Restricted 'Region Code'",
 					Value: region,
 				},
@@ -108,7 +108,7 @@ func main() {
 					fmt.Println(err)
 					os.Exit(2)
 				}
-                // Format output filepath
+				// Format output filepath
 				out, err := gm.OutputFilepath(con)
 				if err != nil {
 					fmt.Println(err)
@@ -123,77 +123,77 @@ func main() {
 				return err
 			},
 		},
-        {
-            Name: "elevation",
-            Aliases: []string{"ev"},
-            Usage: "Google Maps Elevation API Tool",
-            Description: `Accepts an Input CSV File With Latitude, Longitude
-            Pairs and Outputs a Formatted CSV File with Elevation Response
-            Results`,
-            Flags: []cli.Flag{
-                cli.StringFlag{
-                    Name: "key, k",
-                    Usage: "Google Maps Elevation API 'Key'",
-                    Value: apiKey,
-                    EnvVar: "GMAPS_API_KEY",
-                },
-                cli.StringFlag{
-                    Name: "input, i",
-                    Usage: "Input 'Filepath'",
-                    Value: output,
-                },
-                cli.StringFlag{
-                    Name: "output, o",
-                    Usage: "Output 'Directory Path'",
-                    Value: output,
-                },
-            },
-            Action: func(con *cli.Context) (e error) {
-                // Check input arguments
-                err := CheckArgs(con)
-                if err != nil {
-                    fmt.Println(err)
-                    os.Exit(2)
-                }
-                // Establish new Google Maps API client connection
-                clt, err := maps.NewClient(maps.WithAPIKey(con.String("key")))
-                if err != nil{
-                    fmt.Println(err)
-                    os.Exit(2)
-                }
-                // Authenticate client IP
-                err = gm.ElevationTestClientIP(clt)
-                if err != nil {
-                    fmt.Println(err)
-                    os.Exit(2)
-                }
-                // Read in coordinate data from csv file
-                rec, err := gm.ElevationReadCSV(con.String("input"))
-                if err != nil {
-                    fmt.Println(err)
-                    os.Exit(2)
-                }
-                // Request elevations from input csv file records
-                res, err := gm.ElevationRecords(con, clt, rec)
-                if err != nil {
-                    fmt.Println(err)
-                    os.Exit(2)
-                }
-                // Format output filepath
+		{
+			Name:    "elevation",
+			Aliases: []string{"ev"},
+			Usage:   "Google Maps Elevation API Tool",
+			Description: `Accepts an Input CSV File With Latitude, Longitude
+			Pairs and Outputs a Formatted CSV File with Elevation Response
+			Results`,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "key, k",
+					Usage:  "Google Maps Elevation API 'Key'",
+					Value:  apiKey,
+					EnvVar: "GMAPS_API_KEY",
+				},
+				cli.StringFlag{
+					Name:  "input, i",
+					Usage: "Input 'Filepath'",
+					Value: output,
+				},
+				cli.StringFlag{
+					Name:  "output, o",
+					Usage: "Output 'Directory Path'",
+					Value: output,
+				},
+			},
+			Action: func(con *cli.Context) (e error) {
+				// Check input arguments
+				err := CheckArgs(con)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				// Establish new Google Maps API client connection
+				clt, err := maps.NewClient(maps.WithAPIKey(con.String("key")))
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				// Authenticate client IP
+				err = gm.ElevationTestClientIP(clt)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				// Read in coordinate data from csv file
+				rec, err := gm.ElevationReadCSV(con.String("input"))
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				// Request elevations from input csv file records
+				res, err := gm.ElevationRecords(con, clt, rec)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				// Format output filepath
 				out, err := gm.OutputFilepath(con)
 				if err != nil {
 					fmt.Println(err)
 					os.Exit(2)
 				}
-                // Write formatted output to csv file
-                err = gm.ElevationWriteCSV(out, res)
-                if err != nil {
-                    fmt.Println(err)
-                    os.Exit(2)
-                }
-                return err
-            },
-        },
+				// Write formatted output to csv file
+				err = gm.ElevationWriteCSV(out, res)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				return err
+			},
+		},
 	}
 	sort.Sort(cli.FlagsByName(gmaps.Flags))
 	gmaps.Run(os.Args)
